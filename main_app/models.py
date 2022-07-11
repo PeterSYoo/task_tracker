e0303dd9938dff0778390396914754be4bb07d11
Peter Yoo <trustintofu@gmail.com>
2022-07-11 09:24:33 -0700

    wip: adds many to many relationship between users and tasks

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  users = models.ManyToManyField(User, related_name='tasks_set')
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'task_id': self.id})
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return 
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from .models import Task
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello World!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def tasks_index(request):
+  tasks = Task.objects.all()
+  return render(request, 'tasks/index.html', { 'tasks': tasks })
+
+class TaskCreate(CreateView):
+  model = Task
+  fields = ['name', 'description']
+  success_url = '/tasks/'
